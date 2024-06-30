cab0a396483e4cbe2bfca0f061d43aefa8de57f3
nehapatilll <126348166+nehapatilll@users.noreply.github.com>
2024-06-30 17:33:46 +0530

    Create AboutUs.js

---
+++ src/components/AboutUs.js
@@ -0,0 +1,17 @@
+import './Aboutus.css';
+const Aboutus=()=>{
+return (
+    <div>
+        <h1>ABOUT US</h1>
+        <p>
+         Cinemas is the market leader in terms of screen count in Kerala. 
+        The brand has redefined the cinema industry and the way people watch movies in the state. 
+        We are operating at 6 locations and around 21 screens across the state.
+         We have a diversified revenue stream and generate revenues primarily from box office
+          and non-box office which primarily includes revenue from Sale of Food and Beverages, advertisement income, 
+          convenience fees, and income from movie production/ distribution among others.
+        </p>
+    </div>
+)
+}
+export default Aboutus;
